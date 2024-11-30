dcbdcd7f8ce4e121604f70348d66011b358c0f54
zekroTJA <contact@zekro.de>
2024-11-30 11:45:31 +0100

    update and fix some stuff

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,7 +10,7 @@ function run_day {
         TARGET=debug
     fi
     
-    cargo build $RELEASE_FLAG -p "$1"
+    cargo build "$RELEASE_FLAG" -p "$1"
 
     echo -e "\n${C_GREY}----- Test Input -----${C_RESET}"
     if [ -f "$1/test_input.txt" ] && [ -z "$2" ]; then
@@ -27,6 +27,5 @@ if [ -z "$1" ] || [ "$1" == "--test" ]; then
     current_day=$(find . -maxdepth 1 -type d -name "day-*" | sort | tail -1 | cut -c 3-)
     run_day "$current_day" "$1"
 else
-    # cargo run -p $(printf "day-%02d" $1)
     run_day "$(printf "day-%02d" "$1")" "$2"
 fi
