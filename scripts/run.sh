208a08ac596c304d1cebbacbf64d5194b18905da
zekroTJA <contact@zekro.de>
2024-11-30 11:57:08 +0100

    update run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
 C_GREY="\033[38;5;241m"
 C_PINK="\033[38;5;198m"
+C_RED="\033[38;5;9m"
 C_RESET="\033[0m"
 
 function run_day {
@@ -9,8 +10,25 @@ function run_day {
     else 
         TARGET=debug
     fi
+
+    if [ ! -d "$1" ]; then
+        echo -en "${C_RED}error: $1 does not exist.${C_RESET}\n"
+        return
+    fi
     
-    cargo build "$RELEASE_FLAG" -p "$1"
+    # shellcheck disable=SC2086
+    cargo build $RELEASE_FLAG -p "$1"
+
+    if [ "$2" == "--test" ]; then
+        echo -e "\n${C_GREY}----- Test Input -----${C_RESET}"
+        if [ -f "$1/test_input.txt" ]; then
+            "./target/$TARGET/$1" --test
+        else
+            echo -en "${C_RED}error: file $1/test_input.txt does not exist.${C_RESET}\n"
+        fi
+        
+        return
+    fi
 
     echo -e "\n${C_GREY}----- Test Input -----${C_RESET}"
     if [ -f "$1/test_input.txt" ] && [ -z "$2" ]; then
